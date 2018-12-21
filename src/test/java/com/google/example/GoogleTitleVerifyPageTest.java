a7afaa637d607d0471056698cbadc1a13bf7d044
prasadqa <challaprasad772723@gmail.com>
2018-12-21 12:33:55 +0530

    code for jenkins configure

---
+++ src/test/java/com/google/example/GoogleTitleVerifyPageTest.java
@@ -0,0 +1,43 @@
+package com.google.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class GoogleTitleVerifyPageTest 
+{
+	private WebDriver driver;
+	String pageURL = "https://www.google.com/";
+	
+	@BeforeMethod
+	public void setUpBeforeTest()
+	{
+
+        System.setProperty("webdriver.chrome.driver","D:\\SeleniumSamples\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get(pageURL);
+	}
+	
+	@Test
+	public void titleVerify()
+	{
+		String actTitle = driver.getTitle();
+		Assert.assertEquals(actTitle, "Google");
+	}
+	
+	
+	
+	
+	@AfterMethod
+	public void setUpAfterTest()
+	{
+
+        driver.quit();
+	}
+
+	
+			
+}
